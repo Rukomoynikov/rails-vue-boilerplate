9425416a343e3d19992faecbcf84f7717205ac65
Maksim Rukomoynikov <m.rukomoynikov@etpgpb.ru>
2018-08-24 11:08:49 +0300

    Линтер для vue/js файлов

--- app/javascript/components/pages/Home.vue
+++ app/javascript/components/pages/Home.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-      Home
-    </div>
+  <div>
+    Home
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
-    }
+export default {
+  name: 'Home'
+}
 </script>
 
 <style scoped>
--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -12,5 +12,4 @@ const router = new VueRouter({
   routes
 })
 
-
 export default router
